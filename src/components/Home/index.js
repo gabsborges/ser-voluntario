ada07c8afc4ec510035abfcb662930d869d673a5
Gabriel Borges <gabriel.borges_96@hotmail.com>
2023-10-27 13:24:31 -0300

    responsive done

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function InitializePage() {
+    return (
+        <div className='initializePage'>
+            <div className='initializePage-box'>
+                <img src='./logo.png' alt='Imagem de fundo' />
+                <h1>Grandes coisas estão chegando. Novidades em breve!!</h1>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import '../App.css'
+import InitializePage from '../components/Home'
+
+export default function Home() {
+    return (
+        <>
+            <InitializePage/>
+        </>
+    )
+}
